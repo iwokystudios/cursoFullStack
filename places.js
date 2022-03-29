c92c165b384fcb9de9a93f9eee4fd727d0884b70
uabnina <102513629+uabnina@users.noreply.github.com>
2022-03-29 14:21:08 +0200

    cambios

--- places.js
+++ places.js
@@ -1,33 +1,33 @@
-const places = [{
-        name: "Barcelona",
-        img: "barcelona.jpg"
-    },
-    {
-        name: "Londres",
-        img: "londres.jpg"
-    },
-    {
-        name: "París",
-        img: "paris.jpg"
-    }
-];
-
-function placeTemplate(place) {
-    return `
-    <div>
-        <div>
-            <img width='100px' src='${place.img}'/>
-        </div>
-        <div>
-        ${place.name}
-        </div>
-    </div>
-    `;
-}
-
-function init() {
-    const main = document.getElementById("main");
-    places.map(item => main.innerHTML += placeTemplate(item));
-}
-
-window.onload = init;
+// const places = [{
+// name: "Barcelona",
+// img: "barcelona.jpg"
+// },
+// {
+// name: "Londres",
+// img: "londres.jpg"
+// },
+// {
+// name: "París",
+// img: "paris.jpg"
+// }
+// ];
+// 
+// function placeTemplate(place) {
+// return `
+// <div>
+// <div>
+// <img width='100px' src='${place.img}'/>
+// </div>
+// <div>
+// ${place.name}
+// </div>
+// </div>
+// `;
+// }
+// 
+// function init() {
+// const main = document.getElementById("main");
+// places.map(item => main.innerHTML += placeTemplate(item));
+// }
+// 
+// window.onload = init;
