a195506dee971d95fb82210205e226432ed5ef65
LeirbaG20 <102021254+LeirbaG20@users.noreply.github.com>
2022-03-29 13:48:17 +0200

    he realizado cambios

---
+++ places.js
@@ -0,0 +1,26 @@
+const places = [{ name: "Barcelona", img: "barcelona.jpg" },
+{ name: "Londres", img: "londres.jpg" },
+{ name: "Paris", img: "paris.jpg" }];
+function placeTemplate(place) {
+
+    return main.innerHTML = `<div>
+    <div> <img width='350px' src='${place.img}'/></div>
+    <div>${place.name}</div>
+    </div>`;
+    
+}
+function init() {
+    const main = document.getElementById("main");
+
+    places.map((place, placeIndex) => {
+        main.innerHTML += placeTemplate(place);
+    });
+}
+window.onload = init;
+// const main = document.getElementById("main");
+//     places.map((marca, index) => {
+//         main.innerHTML = `<div>
+//         <div> <img width='350px' src='${places[index].img}'/></div>
+//         <div>${places[index].name}</div>
+//         </div>`;
+//     });
