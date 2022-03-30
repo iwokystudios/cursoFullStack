974ecc80369db6de16f2e52747c55f573a7bca9b
JohnJLM <102513934+JohnJLM@users.noreply.github.com>
2022-03-30 14:15:13 +0200

    m

--- public/places.js
+++ public/places.js
@@ -12,21 +12,20 @@ const places =  [
 
 function placeTemplate(place) {
     return `
-    <div class="card">
-        <div class="contentImg">
-            <img src='${place.img}'/>
+    <div>
+        <div>
+            <img width='100px' src='${place.img}'/>
         </div>
-        <div class="content">
+        <div>
         ${place.name}
         </div>
-    </div>    
+    </div>
     `;
 }
 
-
 function init() {
     const main = document.getElementById("main");
-    // places.map(item => main.innerHTML += placeTemplate(item));
+    places.map(item => main.innerHTML += placeTemplate(item));
 }
 
 window.onload = init;
