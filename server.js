e9703de2046f5aac7783bf6628f21a81d2051e6b
alexzgp <alexpriv.bcn@gmail.com>
2022-03-30 13:05:46 +0200

    hola

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/products', (req, res) => {
+    const color = req.query.color;
+
+    if (color === "rojo") {
+        res.send(`¿Me pides algo ${req.query.color}?`)
+    } else {
+        res.send(`Estos son los productos ${req.query.id}`)
+    }
+  
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
