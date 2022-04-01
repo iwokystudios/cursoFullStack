ce279b697183b80f0f2af807c92d9724e91ceebc
Pol Lazaro <102426647+LazaroPol@users.noreply.github.com>
2022-04-01 13:31:05 +0200

    subo los cambios de funciones ciudades

--- public/funciones/funciones.js
+++ public/funciones/funciones.js
@@ -12,16 +12,16 @@
         { nombre: "Pepito", edat: 25 }
     nombreNuevo ----- string
 */
-function cambiaNombre( alumno, nombreNuevo ) {
-    console.log( "Parámetro alumno : ", alumno);
-    console.log( "Parámetro nombreNuevo : ", nombreNuevo);
-    alumno.nombre = nombreNuevo;
-    alumno.edat = 20;
-}
+// function cambiaNombre( alumno, nombreNuevo ) {
+//     console.log( "Parámetro alumno : ", alumno);
+//     console.log( "Parámetro nombreNuevo : ", nombreNuevo);
+//     alumno.nombre = nombreNuevo;
+//     alumno.edat = 20;
+// }
 
-const objetoPepito = { nombre: "Juanito", edat: 25 };
-const nombreNuevoDePepe = "Pepe";
-const objetoPaco = { nombre: "Paquito", edat: 25 };
+// const objetoPepito = { nombre: "Juanito", edat: 25 };
+// const nombreNuevoDePepe = "Pepe";
+// const objetoPaco = { nombre: "Paquito", edat: 25 };
 
 // cambiaNombre( objetoPepito, nombreNuevoDePepe );
 // console.log("ESTOY FUERA DE LA FUNCIÓN AHORA MISMO");
@@ -32,45 +32,62 @@ const objetoPaco = { nombre: "Paquito", edat: 25 };
 // console.log("objetoPaco ahora es : ", objetoPaco);
 
 
-/** FUNCIÓN QUE MODIFICA PROPIEDADES DE OBJETOS DE UNA LISTA DE OBJETOS */
-const listaAlumnos = [
-    { nombre: "Paquito", edad: 25 },
-    { nombre: "Pepito", edad: 25 },
-    { nombre: "Juanito", edad: 25 }
-];
-const cambiaEdadAlumnos = (lista, nuevaEdad) => {
-    console.log(" ESTOY DENTRO DE CAMBIA EDAD ALUMNOS ");
-    lista.map( (alumno) => {
-        if ( alumno.nombre === "Pepito" || alumno.nombre === "Juanito" ) {
-            alumno.edad = nuevaEdad;
-        } else {
-            alumno.edad = alumno.edad + 10;
-        }
-    } );
-    return "hola"
+// /** FUNCIÓN QUE MODIFICA PROPIEDADES DE OBJETOS DE UNA LISTA DE OBJETOS */
+// const listaAlumnos = [
+//     { nombre: "Paquito", edad: 25 },
+//     { nombre: "Pepito", edad: 25 },
+//     { nombre: "Juanito", edad: 25 }
+// ];
+// const cambiaEdadAlumnos = (lista, nuevaEdad) => {
+//     console.log(" ESTOY DENTRO DE CAMBIA EDAD ALUMNOS ");
+//     lista.map( (alumno) => {
+//         if ( alumno.nombre === "Pepito" || alumno.nombre === "Juanito" ) {
+//             alumno.edad = nuevaEdad;
+//         } else {
+//             alumno.edad = alumno.edad + 10;
+//         }
+//     } );
+//     return "hola"
+// };
+// cambiaEdadAlumnos(listaAlumnos, 75);
+
+// (lista = listaAlumnos, nuevaEdad = 75) => {
+//     console.log(" ESTOY DENTRO DE CAMBIA EDAD ALUMNOS ");
+//     lista.map( (alumno) => {
+//         if ( alumno.nombre === "Pepito" || alumno.nombre === "Juanito" ) {
+//             alumno.edad = nuevaEdad;
+//         } else {
+//             alumno.edad = alumno.edad + 10;
+//         }
+//     } );
+//     return "hola"
+// }
+
+
+
+
+
+
+
+
+const casa = {                                               /*Realizo un objeto con 2 atributos.*/
+    direccion: "Avenidotonta",
+    ciudad: "Barcelona"
 };
-cambiaEdadAlumnos(listaAlumnos, 75);
 
-(lista = listaAlumnos, nuevaEdad = 75) => {
-    console.log(" ESTOY DENTRO DE CAMBIA EDAD ALUMNOS ");
-    lista.map( (alumno) => {
-        if ( alumno.nombre === "Pepito" || alumno.nombre === "Juanito" ) {
-            alumno.edad = nuevaEdad;
-        } else {
-            alumno.edad = alumno.edad + 10;
-        }
-    } );
-    return "hola"
-}
+    const ciudadEscojida = "Madrid";                            /*Guardo el valor que quiero añadir en una variable.*/
+
+    const cambiarCiudad = (vieja, nueva) => {                       /*CREO UNA FUNCUON*/
+        vieja.ciudad = nueva;                                          
 
 
+    }
+    cambiarCiudad(casa, ciudadEscojida);                            /*LLAMO A LA FUNCION*/
+    
+    console.log(casa);                                                 /*CONSOLE LOG Y EL OBJETO QUE QUIERA VISUALIZAR*/
 
 
-
-
-
-
-
+/*IMPORTANTISIMO JODER VIEJA Y NUEVA EN ESTE CASO, SERAN SUSTIUDAS INTERNAMENTE POR LOS VALORES CASA Y CIUDAD ESCOJIDA*/
 
 
 
