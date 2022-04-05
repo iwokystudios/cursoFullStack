26087c52cf1d58eb1712131c12b0bab4dddf5ff1
uabnina <uabnina@gmail.com>
2022-04-05 10:49:03 +0200

    cambios

--- frontAngular/src/app/app.component.ts
+++ frontAngular/src/app/app.component.ts
@@ -6,10 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-<<<<<<< HEAD
-  title = 'frontAngular';
-=======
   heroe = "SPIDERMAN";
   villano = "VENOM";
->>>>>>> main
 }
--- frontAngular/src/model/hero.ts
+++ frontAngular/src/model/hero.ts
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
-hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-=======
 export interface Hero {
     id: number;
     name: string;
->>>>>>> main
 }
