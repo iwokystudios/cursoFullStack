c5b121ee826962f6ab2b58a38d2339a1105b4c41
Pol Lazaro <102426647+LazaroPol@users.noreply.github.com>
2022-04-05 11:34:09 +0200

    cambios dos

--- cursoAngular/src/app/app.module.ts
+++ cursoAngular/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
