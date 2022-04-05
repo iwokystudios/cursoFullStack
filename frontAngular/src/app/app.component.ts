0811e3564fcc4448e260332da45112678e3ee5ef
NovoaMaggie <maggiegisell@gmail.com>
2022-04-05 13:23:50 +0200

    últimos cambios maggie

--- frontAngular/src/app/app.component.ts
+++ frontAngular/src/app/app.component.ts
@@ -6,11 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-<<<<<<< HEAD
-  heroe = 'SPIDERMAN';
-  villano = 'VENOM'
-=======
   heroe = "SPIDERMAN";
   villano = "VENOM";
->>>>>>> 5864f9d7fe33aaf4d3ae8fb87043147c73420d99
 }
--- frontAngular/src/app/heroes/heroes.component.ts
+++ frontAngular/src/app/heroes/heroes.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HEROES } from 'src/mocks/mock-heroes';
-<<<<<<< HEAD
-=======
 import { Hero } from 'src/model/hero';
->>>>>>> 5864f9d7fe33aaf4d3ae8fb87043147c73420d99
 
 
 @Component({
@@ -12,27 +9,16 @@ import { Hero } from 'src/model/hero';
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
-<<<<<<< HEAD
-    heroes = HEROES; 
-    selectedHero?: Hero;
-=======
   laListaDeHeroes = HEROES;
   selectedHero?: Hero;
->>>>>>> 5864f9d7fe33aaf4d3ae8fb87043147c73420d99
 
   constructor() { }
 
   ngOnInit(): void {
-<<<<<<< HEAD
-  } 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-=======
   }
 
   onSelect(dory: Hero): void {
     this.selectedHero = dory;
->>>>>>> 5864f9d7fe33aaf4d3ae8fb87043147c73420d99
   }
 
 }
