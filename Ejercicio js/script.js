eb4e87d440ef001a2a1a38dfd2fc27bb85b125d4
alexzgp <alexpriv.bcn@gmail.com>
2022-04-07 14:09:36 +0200

    07/04

---
+++ Ejercicio js/script.js
@@ -0,0 +1,12 @@
+var listaEmpleados = [
+    {"name":"Julio", "age":35, "salary":900},
+    {"name":"Julio", "age":25, "salary":700},
+    {"name":"Julio", "age":23, "salary":1900},
+    {"name":"Julio", "age":30, "salary":1500},
+    {"name":"Julio", "age":19, "salary":800},
+    {"name":"Julio", "age":29, "salary":1400},
+    {"name":"Julio", "age":20, "salary":1100},
+    {"name":"Julio", "age":31, "salary":900},
+    {"name":"Julio", "age":27, "salary":1500},
+    {"name":"Julio", "age":32, "salary":1700},
+]
