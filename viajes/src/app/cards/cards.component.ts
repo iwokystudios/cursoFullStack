40849691d5003d22699f2d8a2798617bdc31455e
alexzgp <alexpriv.bcn@gmail.com>
2022-04-07 10:53:43 +0200

    07/04

--- viajes/src/app/cards/cards.component.ts
+++ viajes/src/app/cards/cards.component.ts
@@ -7,10 +7,10 @@ import { Card } from '../card';
   styleUrls: ['./cards.component.scss']
 })
 export class CardsComponent implements OnInit {
-  card: Card = {
-    precio: 30,
-    ciudad: 'Barcelona',
-    img: 'https://images.pexels.com/photos/1388030/pexels-photo-1388030.jpeg?cs=srgb&dl=pexels-aleksandar-pasaric-1388030.jpg&fm=jpg'
+  @Input() card?: Card = {
+    "ciudad" : "string",
+    "precio" : "number",
+    "img" : "string"
   };
   constructor() { }
 
