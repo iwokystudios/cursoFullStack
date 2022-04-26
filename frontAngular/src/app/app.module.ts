4f2ea8392f460d90a86135a7b459b29dc1bc148e
AMIRKLK <102515443+AMIRKLK@users.noreply.github.com>
2022-04-26 13:51:08 +0200

    Merge branch 'main' into Gelato

--- frontAngular/src/app/app.module.ts
+++ frontAngular/src/app/app.module.ts
@@ -1,23 +1,32 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { CardComponent } from './card/card.component';
+import { MenuComponent } from './menu/menu.component';
+import { ButtonComponent } from './button/button.component';
+import { TravelListComponent } from './travel-list/travel-list.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { FormsModule } from '@angular/forms';
+import { FormProductComponent } from './form-product/form-product.component';
+import { FormFieldComponent } from './form-field/form-field.component';
+import { FormButtonComponent } from './form-button/form-button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent,
-    HeroDetailComponent
+    CardComponent,
+    MenuComponent,
+    ButtonComponent,
+    TravelListComponent,
+    ProductListComponent,
+    FormProductComponent,
+    FormFieldComponent,
+    FormButtonComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
